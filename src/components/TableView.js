33376a06f32ceef22390f3b5ed81879cc5ee824d
nicogarcia98 <nicolasgarcia2809@gmail.com>
2022-02-01 13:22:36 -0300

    creando componentes para estilar

---
+++ src/components/TableView.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+
+
+export default function TableView({option}) {
+
+    useEffect(() => {
+        console.log(option)
+    })
+    
+    return (
+        <div className="container-table">
+            asd
+        </div>
+        )
+
+}
